11cbc858ff7c3e5868961bd18af3133900fb3119
shahbaz123-gif <sajjadahmadjan30@gmail.com>
2025-10-28 22:04:54 +0500

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,51 @@
+# import necessary packages
+from tkinter import *
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+#setup root window
+window = Tk()
+window.title("Codingal's text editor")
+window.geometry("600x500")
+window.rowconfigure(0, minsize=800, weight=1)
+window.columnconfigure(1, minsize=800, weight=1)
+
+#function to open a file
+def open_file():
+    """Open a file for editing."""
+    filepath = askopenfilename(
+        filetypes=[("text files", "*.txt"), ("All files", "*.*")]
+    )
+    if not filepath:
+        return
+    txt_edit.delete(1.0, END)
+    #if a file is opened then display the contents of the file
+    with open(filepath, "r") as input_file:
+        text = input_file.read()
+        txt_edit.insert(END, text)
+        input_file.close()
+    window.title(f"codingal text editor - {filepath}")
+
+def save_file():
+    filepath = asksaveasfilename(
+        defaultextension="txt",
+        filetypes=[("text files", "*.txt"), ("All files", "*.*")],
+    )  
+    if not filepath:
+        return
+    with open(filepath, "w") as output_file:
+        text = txt_edit.get(1.0, END)
+        output_file.write(text)
+    window.title(f"codingal text editor - {filepath}")
+
+txt_edit = Text(window)
+fr_buttons = Frame(window, relief=RAISED, bd=2) 
+btn_open = Button(fr_buttons, text="Open", command=open_file) 
+btn_save = Button(fr_buttons, text="Save As...", command=save_file) 
+
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+
+fr_buttons.grid(row=0, column=0, sticky="ns")
+txt_edit.grid(row=0, column=1, sticky="nsew")
+
+window.mainloop()
